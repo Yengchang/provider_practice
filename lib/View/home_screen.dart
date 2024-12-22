fb6a01bcf7185b75ee8facec38e157274cadf1fc
Chayengchang <134120181+Yengchang@users.noreply.github.com>
2024-12-22 21:46:52 +0700

    Update home_screen.dart

--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -13,6 +13,39 @@ class HomeScreen extends StatelessWidget {
         title: const Text("Expense Tracker App"),
         centerTitle: true,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       body: const Column(
         children: [
           SummuryCard(),
